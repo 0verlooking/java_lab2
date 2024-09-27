ba6a260259217a9b62a742882884344cb2838eb9
Heredfo <pouhtlh@gmail.com>
2024-09-27 21:57:39 +0300

    Init

---
+++ src/main/java/com/chmnu_ki_123/k3/Unicode.java
@@ -0,0 +1,18 @@
+package com.edu.chmnu.ki_123.c3;
+
+public class Unicode {
+    public static void main(String[] args) {
+        char sunSymbol = '\u2609';
+        char chessKing = '\u2654';
+        char musicalNote = '\u266B';
+        char smilingFace = '\u263A';
+        char yinYang = '\u262F';
+
+        System.out.println("Stored Unicode Characters using Escape Sequences:");
+        System.out.println("Sun Symbol: " + sunSymbol);
+        System.out.println("Chess King: " + chessKing);
+        System.out.println("Musical Note: " + musicalNote);
+        System.out.println("Smiling Face: " + smilingFace);
+        System.out.println("Yin Yang: " + yinYang);
+    }
+}
